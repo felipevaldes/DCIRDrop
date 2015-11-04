e90b0a82c15b32a5cc2b498572028cd4e9915538
Felipe Valdes <felipe.valdes@gmail.com>
2015-11-04 09:03:35 -0300

    Two additions to cemDef: 1. cemINT8 & UINT: long ling int with at least 64 bits in size. 2. clean() and clean_array() functions for general use

--- src/cemCommon/cemTypes.h
+++ src/cemCommon/cemTypes.h
@@ -3,6 +3,12 @@
 
 #pragma once
 #include <complex>
+#include <stdint.h>
+
+
+
+#define CLEAN(p) { delete p; p=NULL; }
+#define CLEAN_ARRAY(p) { delete [] p; p=NULL; }
 
 namespace cem_def
 {
@@ -17,6 +23,17 @@ typedef unsigned int cemUINT;
 typedef std::complex<cemFLOAT> cemFCOMPLEX;
 typedef std::complex<cemDOUBLE> cemDCOMPLEX;
 typedef size_t cemSIZE;
+
+#if defined(WINDOWS)
+    typedef __int32 cemLINT;
+    typedef __int64     cemINT8;
+    typedef unsigned __int64 cemUINT8;
+#else
+    typedef int32_t cemLINT;
+    typedef long long  cemINT8;
+    typedef unsigned long long cemUINT8;
+#endif
+
 }
 
 
