ce5d019974b3f24bb8d5d664c349cdeabf0ae381
Felipe Valdes <felipe.valdes@gmail.com>
2015-12-05 16:15:21 -0300

    Moved definitions from cemUtils.cpp to cemUtils.h. This change was done because of the problems experienced while linking the templated functions NumberToString.

--- src/cemCommon/cemUtils.h
+++ src/cemCommon/cemUtils.h
@@ -9,21 +9,34 @@
 using namespace cem_def;
 namespace cem_utils
 {
+//************************************************************************************************//
+/** @brief NumberToString : Transforms number into string.
+ * @param x : Number to be transformed
+ * @author Felipe Valdes */
+//************************************************************************************************//
+template<typename T>
+inline std::string NumberToString(T x)
+{
+    std::ostringstream outstring;
+    outstring.precision(6);
+    outstring << x;
+    return outstring.str();
+}
 
 //************************************************************************************************//
-// STRING UTILITIES
+/** @brief NumberToString : Transforms number into string up to a given precision.
+ * @param x : Number to be transformed
+ * @param precision : Number of digits to be transformed into string
+ * @author Felipe Valdes */
 //************************************************************************************************//
-
 template <class T>
-inline std::string NumberToString(T x);
-
-template <class T>
-inline std::string NumberToString(T x, cemUINT precision);
-
-
-
-
-
+inline std::string NumberToString(T x, cemINT precision)
+{
+    std::ostringstream outstring;
+    outstring.precision(precision);
+    outstring << x;
+    return outstring.str();
+}
 }
 
 
